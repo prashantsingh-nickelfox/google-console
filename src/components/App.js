e30cddbbbf6fb086eebe9d402aad08f1e59f1fb7
basir <basir.jafarzadeh@gmail>
2023-07-30 09:45:35 +0330

    Lesson-10-Make-Sidebar-Smart

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,32 @@
 'use client'
 
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import CartSidebar from './CartSidebar'
 import Header from './Header'
 import { useEffect } from 'react'
 import { hideLoading } from '@/redux/slices/cartSlice'
+import { usePathname } from 'next/navigation'
 
 export default function App({ children }) {
   const dispatch = useDispatch()
   useEffect(() => {
     dispatch(hideLoading())
   }, [dispatch])
+  const { cartItems, loading } = useSelector((state) => state.cart)
+  const pathname = usePathname()
 
   return (
     <div>
-      <div className="mr-32">
+      <div
+        className={`${
+          loading
+            ? ''
+            : cartItems.length > 0 &&
+              (pathname === '/' || pathname.indexOf('/product/') >= 0)
+            ? 'mr-32'
+            : ''
+        }`}
+      >
         <Header />
         <main className="p-4">{children}</main>
       </div>
--- src/components/CartSidebar.js
+++ src/components/CartSidebar.js
@@ -1,6 +1,7 @@
 import { addToCart, removeFromCart } from '@/redux/slices/cartSlice'
 import Image from 'next/image'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import { useDispatch, useSelector } from 'react-redux'
 
 export default function CartSidebar() {
@@ -16,8 +17,19 @@ export default function CartSidebar() {
     dispatch(removeFromCart(id))
   }
 
+  const pathname = usePathname()
+
   return (
-    <div className="fixed top-0 right-0 w-32 h-full shadow-lg border-l border-l-gary-700 overflow-scroll">
+    <div
+      className={
+        loading
+          ? ''
+          : cartItems.length > 0 &&
+            (pathname === '/' || pathname.indexOf('/product/') >= 0)
+          ? 'fixed top-0 right-0 w-32 h-full shadow-lg border-l border-l-gary-700 overflow-scroll'
+          : 'hidden'
+      }
+    >
       {loading ? (
         <div className="py-5 px-2">Loading...</div>
       ) : cartItems.length === 0 ? (
